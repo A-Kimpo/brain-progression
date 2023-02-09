83556297b218234122a932add46fa88e1263cb24
A-Kimpo <112566801+A-Kimpo@users.noreply.github.com>
2023-02-09 21:14:19 +0500

    Code fix
    Rename some vars. Add loop, export. Add new directory src/main.js. Code from brain-progression.js move in main.js. File brain-progression.js import main() from main.js now. Update eslintrc.yml

---
+++ src/main.js
@@ -0,0 +1,66 @@
+import { createInterface } from 'readline';
+// создание интерфейса
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+// promise с получением имени игрока
+const requestUserName = () => new Promise((resolve) => {
+  rl.question('Welcome to the Brain Games!\nMay I have your name?\n', (answer) => {
+    console.log(`Hello, ${answer}!`);
+    resolve(answer);
+  });
+});
+// promise с началом игры
+const startRound = (name) => {
+  // создание массива
+  const arr = [];
+  const randomStep = Math.ceil(Math.random(0, 1) * 10); // шаг прогрессии (рандомный)
+  const randomIncr = Math.ceil(Math.random(0, 1) * 10);
+  for (let i = 1; i < 11; i++) { // цикл заполняющий массив
+    arr.push((i * randomStep) + randomIncr);
+  }
+  const arrClone = [...arr]; // клонирование массива
+  const randomHide = Math.round(Math.random() * arrClone.length);
+  arrClone[randomHide] = '..'; // замена числа на '..'
+  const hideNum = arr[randomHide];
+
+  return new Promise((resolve) => {
+    rl.question(`Question: ${arrClone.join(' ')}\n`, (answer) => {
+      console.log(`Your answer: ${answer}`);
+      if (Number(answer) === hideNum) {
+        // верный ответ
+        console.log('Correct!');
+        resolve(true);
+      } else {
+        // неверный ответ
+        console.log(`'${answer}' is wrong answer ;(. Correct answer was '${hideNum}'. Let's try again, ${name}!`);
+        resolve(false);
+      }
+    });
+  });
+};
+
+const main = async () => {
+  console.log('brain-progression\n');
+  // переменная для получения значения из requestUserName()
+  const valueUserName = await requestUserName();
+  console.log('What number is missing in the progression?');
+  // добавление счетчика верных ответов, при 3 верных подряд игрок побеждает
+  let rightAnswCount = 0;
+  console.log(rightAnswCount);
+  do {
+    // eslint-disable-next-line no-await-in-loop
+    if (await startRound(valueUserName) === true) {
+      rightAnswCount += 1;
+      console.log(rightAnswCount);
+    } else {
+      rightAnswCount = 0;
+      console.log(rightAnswCount);
+    }
+  } while (rightAnswCount !== 3);
+  console.log(`Congratulations, ${valueUserName}!`);
+  rl.close();
+};
+
+export { main };
